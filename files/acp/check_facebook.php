f35c62c6bbcd670a5766e49e507deed923fe60e3
torbenbrodt <t.brodt@gmail.com>
2011-03-06 10:27:45 +0000

    optional package for extended login settings

---
+++ files/acp/check_facebook.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author	Torben Brodt
+ * @copyright	2010 easy-coding.de
+ * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
+ */
+if (!function_exists('curl_init')) {
+  throw new SystemException('Facebook needs the CURL PHP extension.');
+}
+if (!function_exists('json_decode')) {
+  throw new SystemException('Facebook needs the JSON PHP extension.');
+}
+
+// try to delete this file
+@unlink(__FILE__);
+?>
--- files/acp/update_facebook.php
+++ files/acp/update_facebook.php
@@ -4,12 +4,12 @@
  * @copyright	2010 easy-coding.de
  * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
  */
-if (!function_exists('curl_init')) {
-  throw new SystemException('Facebook needs the CURL PHP extension.');
-}
-if (!function_exists('json_decode')) {
-  throw new SystemException('Facebook needs the JSON PHP extension.');
-}
+
+// update package name
+$sql = "UPDATE	wcf".WCF_N."_package
+	SET	package = 'de.easy-coding.wcf.facebook'
+	WHERE	package = 'org.gnex.facebook.auth'";
+WCF::getDB()->sendQuery($sql);
 
 // try to delete this file
 @unlink(__FILE__);
