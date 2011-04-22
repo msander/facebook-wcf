11c24849f99f96ea7abe5007472b58ca02d61080
torbenbrodt <t.brodt@gmail.com>
2011-04-22 14:38:40 +0000

    facebook: fix for wbb lite

--- files/acp/check_facebook.php
+++ files/acp/check_facebook.php
@@ -4,11 +4,6 @@
  * @copyright	2010 easy-coding.de
  * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
  */
-// update package name
-$sql = "UPDATE	wcf".WCF_N."_package
-	SET	package = 'de.easy-coding.wcf.facebook'
-	WHERE	package = 'org.gnex.facebook.auth'";
-WCF::getDB()->sendQuery($sql);
 
 if (!function_exists('curl_init')) {
   throw new SystemException('Facebook needs the CURL PHP extension.');
